3465f6864d47971fd8e7bef00b141e8fd0f10f9b
guluglu18 <77696578+guluglu18@users.noreply.github.com>
2023-04-06 13:41:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ function addToCart(element){
         let total = quantity * price
         cartItems.innerHTML += `<div class="cart-single-item">
                                     <h3>${name}</h3>
-                                    <p>${price}$ x ${quantity} = ${total}$</p>
-                                    <button onclick="removeFromCart(this)" class="remove-item">Ukloni</button>
+                                    <p>${price} x ${quantity} = <span>${total}</span>$</p>
+                                    <button onclick="removeFromCard(this)" class="remove-item">Ukloni</button>
                                 </div>`;
         element.innerText = 'Dodato';
         element.setAttribute('disabled', 'true');
@@ -27,11 +27,29 @@ function addToCart(element){
 
 }
 
-function removeFromCart(element){
+function removeFromCard(element){
+    let totalBuy = document.querySelector('.total');
     let mainEl = element.closest('.cart-single-item');
+    let price = mainEl.querySelector('p span').innerText;
+    let name = mainEl.querySelector('h3').innerText;
+    let vegetables = document.querySelectorAll('.single-item');
+    price = parseInt(price);
+    allTotal -= price;
+    if(allTotal != 0){
+    totalBuy = totalBuy.innerHTML = `Ukupno kosta: ${allTotal}$`;
+    }else{
+        totalBuy.innerHTML = "";
+    }
     mainEl.remove();
-
-
+    
+    vegetables.forEach(function (vege){
+        let itemName = vege.querySelector('.si-content h3').innerText
+        if(name == itemName){
+           vege.querySelector('.actions input').value = 0;
+           vege.querySelector('.actions button').innerText = "Dodaj";
+           vege.querySelector('.actions button').removeAttribute('disabled');
+        }
+    });
 }
 
 
